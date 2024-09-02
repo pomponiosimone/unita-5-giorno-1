6a1b6a9a37c5d92426a0c6d767d9d1acf89a8de4
Simone Pomponio <Pomponio671@gmail.com>
2024-09-02 15:53:01 +0200

    Initial commit

---
+++ src/test/java/pomponiosimone/ApplicationTest.java
@@ -0,0 +1,13 @@
+package pomponiosimone;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
