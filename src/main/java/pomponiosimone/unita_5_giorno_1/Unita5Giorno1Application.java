bee1abc1eb9129f04c556057e0cc40b29801728d
Simone Pomponio <Pomponio671@gmail.com>
2024-09-02 15:56:01 +0200

    spring

---
+++ src/main/java/pomponiosimone/unita_5_giorno_1/Unita5Giorno1Application.java
@@ -0,0 +1,13 @@
+package pomponiosimone.unita_5_giorno_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unita5Giorno1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unita5Giorno1Application.class, args);
+	}
+
+}
